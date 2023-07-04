107906aec6e1ad7412f66d0d6ffccc50ad299dc9
unknown <Giovani-lang>
2023-07-04 22:25:09 +0100

    First Commit

---
+++ PI-SCHULE/src/main/java/com/logonedigital/PI/SCHULE/PiSchuleApplication.java
@@ -0,0 +1,13 @@
+package com.logonedigital.PI.SCHULE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiSchuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiSchuleApplication.class, args);
+	}
+
+}
---
+++ PI-SCHULE/src/test/java/com/logonedigital/PI/SCHULE/PiSchuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logonedigital.PI.SCHULE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiSchuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
