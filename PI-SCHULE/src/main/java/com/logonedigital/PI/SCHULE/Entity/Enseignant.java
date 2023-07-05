f0e987dd794f05f2f33778f9584289ff90c6547e
Giovani-lang <tchicayavytika@gmail.com>
2023-07-05 22:47:25 +0100

    adding files

--- PI-SCHULE/src/main/java/com/logonedigital/PI/SCHULE/Entity/Enseignant.java
+++ PI-SCHULE/src/main/java/com/logonedigital/PI/SCHULE/Entity/Enseignant.java
@@ -1,6 +1,7 @@
 package com.logonedigital.PI.SCHULE.Entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,18 +11,36 @@ import lombok.Setter;
 import java.io.Serial;
 import java.io.Serializable;
 
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "tb_enseignants")
-@PrimaryKeyJoinColumn(name = "id")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "tb_utilisateurs")
 @Entity
-public class Enseignant implements Serializable {
+public  class Enseignant implements Serializable {
     @Serial
     private static final Long serialVersionUID = 1L;
     @Id
     private Long id;
+    @Email(message = "invalid email")
+    @Column(unique = true)
+    protected String email;
+    @NotNull(message = "required field")
+    @Column(unique = true)
+    protected String nom;
+    @NotNull(message = "required field")
+    @Column(unique = true)
+    protected String prenom;
+    @NotNull(message = "required field")
+    @Column(unique = true, name = "numero_de_telephone")
+    protected Integer numeroTel;
+    @NotNull(message = "required field")
+    @Column(unique = true)
+    protected String motDePasse;
+    @NotNull
+    private Role role;
     @NotNull
     private String discipline;
 }
